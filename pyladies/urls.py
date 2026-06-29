fc9429d71365e12b3b6a47ce26e3613e54524e6a
Fatimah Nurul Insani <fatimahnurulinsani@gmail.com>
2026-06-29 17:49:28 +0700

    change url pattern for homepage

--- pyladies/urls.py
+++ pyladies/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("sites/", include("sites.urls")),
+    path("", include("sites.urls")),
     path("admin/", admin.site.urls),
 ]
 
