fd69092831f71630b1afd14bf54e77409431f990
Fatimah Nurul Insani <fatimahnurulinsani@gmail.com>
2026-07-28 21:45:27 +0700

    Remove PostgreSQL dependency

--- pyladies/settings.py
+++ pyladies/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 import os
@@ -79,10 +78,10 @@ WSGI_APPLICATION = "pyladies.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default":  dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR}/db.sqlite3',  # default: sqlite buat local
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
